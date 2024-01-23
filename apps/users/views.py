7ec3b92b6b6df12a20fc19e93d8adb030a7d24d9
easy_wu <2418087868@qq.com>
2024-01-23 16:26:26 +0800

    Changes

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,9 +2,8 @@ from apps.users.models import HomeUsers
 from rest_framework import viewsets
 from apps.users.serializer import HomeUsersSerializers
 
+
 # test
 class HomeUsersViewSet(viewsets.ModelViewSet):
     queryset = HomeUsers.objects.all()
     serializer_class = HomeUsersSerializers
-
-
